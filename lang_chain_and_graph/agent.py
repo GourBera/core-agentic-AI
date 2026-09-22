6c41c17bc435b7761f042ef39c20dd39cf86c4af
GourBera <gour.bera.ds@gmail.com>
2026-09-22 23:04:22 +0530

    Todays Learning

--- lang_chain_and_graph/agent.py
+++ lang_chain_and_graph/agent.py
@@ -1,23 +1,11 @@
+# import os
 # from langchain_openai import ChatOpenAI
 
 # llm = ChatOpenAI(
 #     model="gpt-4o-mini",
 #     temperature=0.0,
-#     api_key="voc-178971324916695047539506ab10eb17fa180.59712731",
-#     base_url="https://openai.vocareum.com/v1"
+#     api_key=os.getenv("OPENAI_API_KEY"),
+#     base_url=os.getenv("OPENAI_BASE_URL")
 # )
 
 # print(llm.invoke("Hello, how are you?"))
-
-from langchain_classic.output_parsers import BooleanOutputParser
-
-
-# Initialize the parser
-parser = BooleanOutputParser()
-
-# Standard strings it parses
-print(parser.parse("YES"))    # Returns True
-print(parser.parse("no"))     # Returns False
-print(parser.parse("  true ")) # Returns True
-print(parser.parse("False"))  # Returns False
-print(parser.parse("  YES  ")) # Returns True
